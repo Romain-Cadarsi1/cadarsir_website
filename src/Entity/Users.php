db7d234b46ee166a7f56db33fd0561d2dba794db
Romain <romain.cardasi@etu.umontpellier.fr>
2020-01-24 00:27:53 +0100

    Update Users.php

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -11,6 +11,12 @@ class Users
 {
     /**
      * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
      * @ORM\Column(type="string", length=255)
      */
     private $nom;
@@ -24,6 +30,11 @@ class Users
      * @ORM\Column(type="integer")
      */
     private $age;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getNom(): ?string
     {
