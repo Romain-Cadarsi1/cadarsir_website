25572ece8b7dab6331842a5771d8b733ded11ce7
Cadarsi Romain <romain.cadarsi@etu.umontpellier.fr>
2020-02-13 18:59:19 +0100

    Changement du site

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -36,4 +36,42 @@ class WebsiteController extends AbstractController
         	'message' => "Je crois c'est ok"
         ]);
     }
+
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('website/contact.html.twig', []);
+    }
+
+    /**
+     * @Route("/send", name="Message envoyé")
+     */
+    public function envoyerMail(){
+        $message = "Bonjour Romain Cadarsi, vous avez reçu un message de" . $_POST['nom'] . $_POST['prenom'] . "\n Qui dit : \n " . $_POST['message'];
+        mail('dev@cadarsir.fr', 'Message client', $message);
+        return $this->render('website/message.html.twig', [
+            'message' => "Message envoyé, vous recevrez une réponse sous peu si une réponse est requise.",
+            'slider1' => "Votre message est" , 
+            'slider21' => "En cours d'envoi" ,
+            'slider22' => "Envoyé" ,
+            'slider23' => "Réceptionné"]);
+    }
+
+    /**
+     * @Route("/bepatient", name="Page en cours d\'implémentation")
+     */
+    public function EnCours(){
+        //$message = "Bonjour Romain Cadarsi, vous avez reçu un message de" . $_POST['nom'] . $_POST['prenom'] . "\n Qui dit : \n " . $_POST['message'];
+        //mail('dev@cadarsir.fr', 'Message client', $message);
+        return $this->render('website/message.html.twig', [
+            'message' => "Aïe, il semble que cette page n'est pas encore terminée , revenez bientôt pour la voir ! \n La patience est une vertu.",
+            'slider1' => "Oups, cette page est" , 
+            'slider21' => "Non disponible" ,
+            'slider22' => "En création" ,
+            'slider23' => "Prévu pour bientôt"]);
+    }
+
+
 }
